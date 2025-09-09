294fc84fc91e16c5c0a53094fc7e82cab67d0a9d
Dineshchand <dineshchandgr@gmail.com>
2025-09-09 13:30:35 +0800

    Update CarTest.java

--- programs/OOPS/CarTest.java
+++ programs/OOPS/CarTest.java
@@ -7,6 +7,17 @@ class Car {
     String color;
     int year;
 
+    // no args Constructor
+    Car() {
+
+    }
+
+    // Constructor
+    Car(String brand, String color) {
+        this.brand = brand;
+        this.color = color;
+    }
+
     // Constructor
     Car(String brand, String color, int year) {
         this.brand = brand;
@@ -27,11 +38,25 @@ class Car {
 public class CarTest {
     public static void main(String[] args) {
         // Creating objects of Car class
-        Car car1 = new Car("Toyota", "Red", 2020);
+        //Car car1 = new Car("Toyota", "Red", 2020);
+
+
+        Car car1 = new Car();
         Car car2 = new Car("Honda", "Blue", 2022);
 
         // Calling methods using objects
         car1.displayDetails();
         car2.displayDetails();
+
+
+        car1.brand = "Toyota";
+        car1.year = 2020;
+        car1.color = "Red";
+        car1.displayDetails();
+
+        Car car3 = new Car("Mercedes", "Yellow");
+        car3.displayDetails();
+
+
     }
 }
